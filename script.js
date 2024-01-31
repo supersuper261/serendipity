70dcb4cecdf9a45ac6f53ef1b78f0979a0460a01
supersuper261 <148677532+supersuper261@users.noreply.github.com>
2024-02-01 02:27:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function toggleLetter() {
-  const container = document.querySelector('.container');
-  container.classList.toggle('clicked');
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container");
+
+  container.addEventListener("click", function () {
+    container.classList.toggle("clicked");
+  });
+});
